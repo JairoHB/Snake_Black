e15a4888fda823b9faa5767dc708ca1c198794db
JairoHB <jairotorreshb@outlook.com>
2018-08-31 12:38:14 -0500

    1.0

---
+++ app/src/main/java/com/app/alfano/caro/snake/Login_Screen.java
@@ -0,0 +1,13 @@
+package com.app.alfano.caro.snake;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Login_Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login__screen);
+    }
+}
